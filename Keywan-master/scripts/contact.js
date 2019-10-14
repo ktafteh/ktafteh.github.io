60645418d46130d673f3effe4f142ec1bebb510f
ktafteh <56327965+ktafteh@users.noreply.github.com>
2019-10-13 19:52:24 -0700

    Add files via upload

---
+++ Keywan-master/scripts/contact.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+  // event handlers
+  $("#contact-button").on("click", () => {
+    window.location.href = "mailto:ktafteh@ucsd.edu?subject=Inquiry for Keywan";
+  });
+});
