d47b5241cf9735cfc3300baf790ca4c982159b43
Ryan Grunest <ryangrunest45@gmail.com>
2019-11-23 13:41:49 -0800

    updates from touchup sessions

--- Keywan-master/scripts/contact.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-  // event handlers
-  $("#contact-button").on("click", () => {
-    window.location.href = "mailto:ktafteh@ucsd.edu?subject=Inquiry for Keywan";
-  });
-});
