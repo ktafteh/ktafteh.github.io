456ca89a18fb3b9d2f754dca18eee459e2d765fb
ktafteh <56327965+ktafteh@users.noreply.github.com>
2025-03-05 16:55:36 -0800

    Add files via upload

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   // event handlers
   $("#contact-button").on("click", () => {
-    window.location.href = "mailto:ktafteh@ucsd.edu?subject=Inquiry for Keywan";
+    window.location.href = "mailto:ktaftehart@gmail.com?subject=Inquiry for Keywan";
   });
 });
